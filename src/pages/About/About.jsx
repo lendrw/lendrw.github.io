915c15dc2e696049cc9627aa7c57771822919d3d
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-02-26 14:49:21 -0300

    about section finished

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,20 @@
 import styles from './About.module.css';
+import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
 
 const About = () => {
   return (
     <div 
       className={`container ${styles.about}`}
       id='about'>
-      
+      <h1>Sobre mim</h1>
+      <AnimatedSection id='about' className={styles.animatedSection}> 
+        <p>Sou um desenvolvedor front-end com experiência em React.js, 
+          JavaScript, Styled Components e consumo de APIs. Formado em 
+          Sistemas de Informação pela UFERSA, tenho como objetivo transmitir 
+          experiências agradáveis para os usuários. Sou um profissional 
+          autodidata, estou sempre buscando melhorar minhas habilidades 
+          e aprender novas tecnologias.</p>
+      </AnimatedSection>
     </div>
   );
 };
