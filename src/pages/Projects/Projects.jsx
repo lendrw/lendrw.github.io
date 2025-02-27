1ea81187874fa2456c7e71404ec261935b838ed5
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-02-27 17:15:32 -0300

    starting project section

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,59 @@
 import styles from './Projects.module.css';
+import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
+import secretWord from '../../assets/img/secret_word.png';
 
 const Projects = () => {
   return (
     <div 
       className={`container ${styles.projects}`}
       id='projects'>
-      projects
+      <h1>Projetos</h1>
+      <AnimatedSection id='projects' className={styles.animatedSection}>
+        <div>
+          <a href="">
+            <img src={secretWord} alt=""/>
+          </a>
+          <h2></h2>
+          <p></p>
+          <span></span>
+          <div>
+            Visualizar
+          </div>
+        </div>
+        <div>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <h2></h2>
+          <p></p>
+          <span></span>
+          <div>
+            Visualizar
+          </div>
+        </div>
+        <div>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <h2></h2>
+          <p></p>
+          <span></span>
+          <div>
+            Visualizar
+          </div>
+        </div>
+        <div>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <h2></h2>
+          <p></p>
+          <span></span>
+          <div>
+            Visualizar
+          </div>
+        </div>
+      </AnimatedSection>
     </div>
   )
 }
