9f23b18c177f10612b714799a51780f18199883b
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-03-02 18:19:40 -0300

    contact finished

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,53 @@
 import styles from './Contact.module.css';
+import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
+import { FaGithub, FaLinkedin, FaEnvelope, FaPhone } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div 
       className={`container ${styles.contact}`}
       id='contact'>
-      contact
+      <h1>Contatos</h1>
+      <AnimatedSection id='contatos' className={styles.animatedSection}>
+        <div className={styles.contatoContainer}>
+          <a href="" className={styles.contactLink}>
+            <div>
+              <div className={styles.iconBg}>
+                <FaEnvelope className={styles.contactIcons}/>
+              </div>
+              <p>E-mail</p>
+              <span>leandrocabral321@gmail.com</span>
+            </div>
+          </a>
+          <a href="" className={styles.contactLink}>
+            <div>
+              <div className={styles.iconBg}>
+                <FaPhone className={styles.contactIcons}/>
+              </div>
+              <p>Telefone</p>
+              <span>(84)9 9991-7798</span>
+            </div>
+          </a>
+          <a href="" className={styles.contactLink}>
+            <div>
+              <div className={styles.iconBg}>
+                <FaLinkedin className={styles.contactIcons}/>
+              </div>
+              <p>Linkedin</p>
+              <span>@leandrocabral</span>
+            </div>
+          </a>
+          <a href="" className={styles.contactLink}>
+            <div>
+              <div className={styles.iconBg}>
+                <FaGithub className={styles.contactIcons}/>
+              </div>
+              <p>GitHub</p>
+              <span>@lendrw</span>
+            </div>
+          </a>
+        </div>
+      </AnimatedSection>
     </div>
   )
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import styles from './Projects.module.css';
 import AnimatedSection from '../../components/AnimatedSection/AnimatedSection';
 import secretWord from '../../assets/img/secret_word.png';
+import { FaLink } from 'react-icons/fa';
 
 const Projects = () => {
   return (
@@ -9,47 +10,53 @@ const Projects = () => {
       id='projects'>
       <h1>Projetos</h1>
       <AnimatedSection id='projects' className={styles.animatedSection}>
-        <div>
-          <a href="">
+        <div className={styles.projectCard}>
+          <a href="https://lendrw.github.io/secret_word">
             <img src={secretWord} alt=""/>
           </a>
-          <h2></h2>
-          <p></p>
-          <span></span>
-          <div>
+          <h2>Secret Word</h2>
+          <p>Jogo de palavras</p>
+          <span>Tecnologias utilizadas no projeto: HTML, CSS, JavaScript, React + Vite.</span>
+          <div className={styles.linkButton}>
+          <a href="https://lendrw.github.io/secret_word">
+            <FaLink/>
+            Visualizar
+          </a>
+          </div>
+        </div>
+        <div className={styles.projectCard}>
+          <a href="https://lendrw.github.io/secret_word">
+            <img src={secretWord} alt=""/>
+          </a>
+          <h2>Secret Word</h2>
+          <p>Jogo de palavras</p>
+          <span>Tecnologias utilizadas no projeto: HTML, CSS, JavaScript, React + Vite.</span>
+          <div className={styles.linkButton}>
+            <FaLink/>
             Visualizar
           </div>
         </div>
-        <div>
-          <a href="">
-            <img src="" alt="" />
+        <div className={styles.projectCard}>
+          <a href="https://lendrw.github.io/secret_word">
+            <img src={secretWord} alt=""/>
           </a>
-          <h2></h2>
-          <p></p>
-          <span></span>
-          <div>
+          <h2>Secret Word</h2>
+          <p>Jogo de palavras</p>
+          <span>Tecnologias utilizadas no projeto: HTML, CSS, JavaScript, React + Vite.</span>
+          <div className={styles.linkButton}>
+            <FaLink/>
             Visualizar
           </div>
         </div>
-        <div>
-          <a href="">
-            <img src="" alt="" />
+        <div className={styles.projectCard}>
+          <a href="https://lendrw.github.io/secret_word">
+            <img src={secretWord} alt=""/>
           </a>
-          <h2></h2>
-          <p></p>
-          <span></span>
-          <div>
-            Visualizar
-          </div>
-        </div>
-        <div>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-          <h2></h2>
-          <p></p>
-          <span></span>
-          <div>
+          <h2>Secret Word</h2>
+          <p>Jogo de palavras</p>
+          <span>Tecnologias utilizadas no projeto: HTML, CSS, JavaScript, React + Vite.</span>
+          <div className={styles.linkButton}>
+            <FaLink/>
             Visualizar
           </div>
         </div>
