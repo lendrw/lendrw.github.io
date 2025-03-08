a6248f96cf1e299e8ba5b588cc24d2559b6d43da
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-03-08 14:15:59 -0300

    Update AnimatedSection.jsx
    ajuste para mobile

--- src/components/AnimatedSection/AnimatedSection.jsx
+++ src/components/AnimatedSection/AnimatedSection.jsx
@@ -1,18 +1,26 @@
 import { motion, useAnimation } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const AnimatedSection = ({ id, children, className }) => {
   const controls = useAnimation();
+  const [isMobile, setIsMobile] = useState(false); 
 
   useEffect(() => {
-    controls.start({ opacity: 1, x: 0 });
+    setIsMobile(window.innerWidth <= 768); 
+
+    const controlsStart = () => {
+      controls.start({ opacity: 1, x: 0 });
+    };
+
+    controlsStart();
 
     const handleScroll = () => {
       const element = document.getElementById(id);
       if (!element) return;
 
       const rect = element.getBoundingClientRect();
-      const isVisible = rect.top <= window.innerHeight / 1.5 && rect.bottom >= window.innerHeight / 1.5;
+      const isVisible =
+        rect.top <= window.innerHeight / 1.5 && rect.bottom >= window.innerHeight / 1.5;
 
       if (isVisible) {
         controls.start({ opacity: 1, x: 0 });
@@ -21,14 +29,19 @@ const AnimatedSection = ({ id, children, className }) => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    let throttleTimer;
+    window.addEventListener("scroll", () => {
+      if (throttleTimer) clearTimeout(throttleTimer);
+      throttleTimer = setTimeout(handleScroll, 100);
+    });
+
     return () => window.removeEventListener("scroll", handleScroll);
   }, [controls, id]);
 
   return (
     <div id={id} className={className}>
       <motion.div
-        initial={{ opacity: 0, x: -100 }}
+        initial={{ opacity: 0, x: isMobile ? 0 : -100 }} 
         animate={controls}
         transition={{ duration: 0.5 }}
       >
